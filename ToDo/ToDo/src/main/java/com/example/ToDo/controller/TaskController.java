cb3d7606e005e6f23ecc703e4d70656bc1abc85d
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-07-28 19:10:54 +0300

    added something

--- ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
+++ ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
@@ -6,10 +6,7 @@ import com.example.ToDo.business.responses.TaskResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ public class TaskController {
        return taskService.getTasks();
     }
 
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete(@PathVariable int id){
         if(id == 0){
             return new ResponseEntity<>("Wrong id number", HttpStatus.BAD_REQUEST);
@@ -33,7 +31,8 @@ public class TaskController {
         return new ResponseEntity<>("deleted",HttpStatus.OK);
     }
 
-    public ResponseEntity<String> add(TaskRequest taskRequest){
+    @PostMapping("/add")
+    public ResponseEntity<String> add(@RequestBody TaskRequest taskRequest){
         if (taskService.controlTask(taskRequest)){
             return new ResponseEntity<>("Empty field",HttpStatus.BAD_REQUEST);
         }else {
@@ -42,7 +41,8 @@ public class TaskController {
         }
     }
 
-    public ResponseEntity<String> update(TaskRequest taskRequest){
+    @PostMapping("/update")
+    public ResponseEntity<String> update(@RequestBody TaskRequest taskRequest){
         if (taskService.controlTask(taskRequest)){
             return new ResponseEntity<>("Empty field",HttpStatus.BAD_REQUEST);
         }else {
