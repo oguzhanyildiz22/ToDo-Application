3d398d65034cc22070d603748890db18d26104c6
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-07-28 17:59:15 +0300

    Added delete method

--- ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
+++ ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
@@ -3,7 +3,10 @@ package com.example.ToDo.controller;
 import com.example.ToDo.business.abstracts.TaskService;
 import com.example.ToDo.business.responses.TaskResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,14 @@ public class TaskController {
     @GetMapping("/list")
     public List<TaskResponse> getAllTasks(){
        return taskService.getTasks();
+    }
+
+    public ResponseEntity<String> delete(@PathVariable int id){
+        if(id == 0){
+            return new ResponseEntity<>("Wrong id number", HttpStatus.BAD_REQUEST);
+        }
+        taskService.deleteTask(id);
+        return new ResponseEntity<>("deleted",HttpStatus.OK);
     }
 
 }
