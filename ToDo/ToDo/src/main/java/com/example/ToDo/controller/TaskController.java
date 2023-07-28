83dcbf8d4e0223856267a1840edb23f3ca8ac8f8
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-07-28 17:46:08 +0300

    Added getAllTasks method to list tasks.

--- ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
+++ ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
@@ -1,12 +1,24 @@
 package com.example.ToDo.controller;
 
 import com.example.ToDo.business.abstracts.TaskService;
+import com.example.ToDo.business.responses.TaskResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/task")
 public class TaskController {
 
+    @Autowired
     private TaskService taskService;
 
+    @GetMapping("/list")
+    public List<TaskResponse> getAllTasks(){
+       return taskService.getTasks();
+    }
 
 }
