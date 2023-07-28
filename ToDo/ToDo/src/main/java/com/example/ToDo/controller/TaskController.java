991c94e5cfcc7d4dc5c98fce6f30880b14dafcaa
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-07-28 17:42:27 +0300

    Created TaskController class.

---
+++ ToDo/ToDo/src/main/java/com/example/ToDo/controller/TaskController.java
@@ -0,0 +1,12 @@
+package com.example.ToDo.controller;
+
+import com.example.ToDo.business.abstracts.TaskService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    private TaskService taskService;
+
+
+}
